2163f2d28c58251fdaa3c7ebc82a8502d1492105
Vidhya Kumar <vidhya.kumar@outlook.com>
2023-12-12 17:48:30 +0800

    feat: test suspense skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
-import { Pokemon } from "@/components/pokemon";
-import Pokedex from "pokedex-promise-v2";
-export default async function Home() {
-  const P = new Pokedex();
-  const pokemons = await P.getPokemonsList({ limit: 24, offset: 0 });
+import Pokedex from "pokedex-promise-v2"
+
+import { Pokemon } from "@/components/pokemon"
+
+interface HomePageProps {
+  searchParams: {
+    name: string
+  }
+}
+
+export default async function Home({ searchParams }: HomePageProps) {
+  const P = new Pokedex()
+  const pokemons = await P.getPokemonsList({ limit: 24, offset: 0 })
+  await new Promise((resolve) => setTimeout(resolve, 1000))
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
@@ -12,5 +21,5 @@ export default async function Home() {
         ))}
       </div>
     </main>
-  );
+  )
 }
