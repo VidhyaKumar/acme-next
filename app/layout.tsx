81953ca712f7b5817e8ba9a296515cf2caf52b3f
Vidhya Kumar <vidhya.kumar@outlook.com>
2023-12-13 18:37:39 +0800

    feat: update font configuration and minor card style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
 
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "VK Pokedex",
@@ -16,8 +21,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
--- components/pokemon.tsx
+++ components/pokemon.tsx
@@ -14,7 +14,7 @@ export async function Pokemon({ name }: PokemonProps) {
   return (
     <div
       className={cn(
-        "flex flex-col items-center border-4 p-8 rounded-lg justify-center aspect-[10/12]",
+        "flex flex-col items-center border-4 rounded-lg justify-center aspect-[10/12]",
         pokemon.types[0].type.name === "grass" && "border-green-400",
         pokemon.types[0].type.name === "fire" && "border-red-400",
         pokemon.types[0].type.name === "water" && "border-blue-400",
@@ -37,17 +37,47 @@ export async function Pokemon({ name }: PokemonProps) {
         pokemon.types[0].type.name === "unknown" && "border-gray-400"
       )}
     >
-      <Image
-        src={
-          pokemon.sprites.other["official-artwork"].front_default ||
-          "/images/pokeball.svg"
-        }
-        width={120}
-        height={120}
-        className="object-contain object-center"
-        alt={pokemon.name}
-      />
-      <h1 className="text-xl capitalize text-center mt-2">{pokemon.name}</h1>
+      <div className="w-full flex-1 flex justify-center items-center p-8">
+        <Image
+          src={
+            pokemon.sprites.other["official-artwork"].front_default ||
+            "/images/pokeball.svg"
+          }
+          width={120}
+          height={120}
+          className="object-contain object-center"
+          alt={pokemon.name}
+        />
+      </div>
+      <div
+        className={cn(
+          "w-full py-4 px-8",
+          pokemon.types[0].type.name === "grass" && "bg-green-400",
+          pokemon.types[0].type.name === "fire" && "bg-red-400",
+          pokemon.types[0].type.name === "water" && "bg-blue-400",
+          pokemon.types[0].type.name === "electric" && "bg-yellow-400",
+          pokemon.types[0].type.name === "poison" && "bg-purple-400",
+          pokemon.types[0].type.name === "bug" && "bg-green-400",
+          pokemon.types[0].type.name === "flying" && "bg-blue-400",
+          pokemon.types[0].type.name === "normal" && "bg-gray-400",
+          pokemon.types[0].type.name === "ground" && "bg-yellow-400",
+          pokemon.types[0].type.name === "fairy" && "bg-pink-400",
+          pokemon.types[0].type.name === "fighting" && "bg-red-400",
+          pokemon.types[0].type.name === "psychic" && "bg-pink-400",
+          pokemon.types[0].type.name === "rock" && "bg-gray-400",
+          pokemon.types[0].type.name === "steel" && "bg-gray-400",
+          pokemon.types[0].type.name === "ice" && "bg-blue-400",
+          pokemon.types[0].type.name === "ghost" && "bg-purple-400",
+          pokemon.types[0].type.name === "dragon" && "bg-purple-400",
+          pokemon.types[0].type.name === "dark" && "bg-gray-400",
+          pokemon.types[0].type.name === "shadow" && "bg-gray-400",
+          pokemon.types[0].type.name === "unknown" && "bg-gray-400"
+        )}
+      >
+        <h1 className="text-xl capitalize text-center text-slate-900">
+          {pokemon.name}
+        </h1>
+      </div>
     </div>
   )
 }
