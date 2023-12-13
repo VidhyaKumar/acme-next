b96e10f31b2989b287b8e3f0b544de200b9e711a
Vidhya Kumar <vidhya.kumar@outlook.com>
2023-12-13 21:46:48 +0800

    style: update border and background colors in Pokemon component

--- components/pokemon.tsx
+++ components/pokemon.tsx
@@ -16,25 +16,25 @@ export async function Pokemon({ name }: PokemonProps) {
       className={cn(
         "flex flex-col items-center border-4 rounded-lg justify-center aspect-[10/12]",
         pokemon.types[0].type.name === "grass" && "border-green-400",
-        pokemon.types[0].type.name === "fire" && "border-red-400",
+        pokemon.types[0].type.name === "fire" && "border-amber-400",
         pokemon.types[0].type.name === "water" && "border-blue-400",
         pokemon.types[0].type.name === "electric" && "border-yellow-400",
         pokemon.types[0].type.name === "poison" && "border-purple-400",
-        pokemon.types[0].type.name === "bug" && "border-green-400",
-        pokemon.types[0].type.name === "flying" && "border-blue-400",
+        pokemon.types[0].type.name === "bug" && "border-teal-400",
+        pokemon.types[0].type.name === "flying" && "border-sky-400",
         pokemon.types[0].type.name === "normal" && "border-gray-400",
-        pokemon.types[0].type.name === "ground" && "border-yellow-400",
+        pokemon.types[0].type.name === "ground" && "border-orange-400",
         pokemon.types[0].type.name === "fairy" && "border-pink-400",
         pokemon.types[0].type.name === "fighting" && "border-red-400",
         pokemon.types[0].type.name === "psychic" && "border-pink-400",
-        pokemon.types[0].type.name === "rock" && "border-gray-400",
-        pokemon.types[0].type.name === "steel" && "border-gray-400",
-        pokemon.types[0].type.name === "ice" && "border-blue-400",
+        pokemon.types[0].type.name === "rock" && "border-stone-400",
+        pokemon.types[0].type.name === "steel" && "border-zinc-400",
+        pokemon.types[0].type.name === "ice" && "border-cyan-400",
         pokemon.types[0].type.name === "ghost" && "border-purple-400",
         pokemon.types[0].type.name === "dragon" && "border-purple-400",
         pokemon.types[0].type.name === "dark" && "border-gray-400",
         pokemon.types[0].type.name === "shadow" && "border-gray-400",
-        pokemon.types[0].type.name === "unknown" && "border-gray-400"
+        pokemon.types[0].type.name === "unknown" && "border-white"
       )}
     >
       <div className="w-full flex-1 flex justify-center items-center p-8">
@@ -51,27 +51,27 @@ export async function Pokemon({ name }: PokemonProps) {
       </div>
       <div
         className={cn(
-          "w-full py-4 px-8",
+          "w-full pt-4 pb-3 px-8",
           pokemon.types[0].type.name === "grass" && "bg-green-400",
-          pokemon.types[0].type.name === "fire" && "bg-red-400",
+          pokemon.types[0].type.name === "fire" && "bg-amber-400",
           pokemon.types[0].type.name === "water" && "bg-blue-400",
           pokemon.types[0].type.name === "electric" && "bg-yellow-400",
           pokemon.types[0].type.name === "poison" && "bg-purple-400",
-          pokemon.types[0].type.name === "bug" && "bg-green-400",
-          pokemon.types[0].type.name === "flying" && "bg-blue-400",
+          pokemon.types[0].type.name === "bug" && "bg-teal-400",
+          pokemon.types[0].type.name === "flying" && "bg-sky-400",
           pokemon.types[0].type.name === "normal" && "bg-gray-400",
-          pokemon.types[0].type.name === "ground" && "bg-yellow-400",
+          pokemon.types[0].type.name === "ground" && "bg-orange-400",
           pokemon.types[0].type.name === "fairy" && "bg-pink-400",
           pokemon.types[0].type.name === "fighting" && "bg-red-400",
           pokemon.types[0].type.name === "psychic" && "bg-pink-400",
-          pokemon.types[0].type.name === "rock" && "bg-gray-400",
-          pokemon.types[0].type.name === "steel" && "bg-gray-400",
-          pokemon.types[0].type.name === "ice" && "bg-blue-400",
+          pokemon.types[0].type.name === "rock" && "bg-stone-400",
+          pokemon.types[0].type.name === "steel" && "bg-zinc-400",
+          pokemon.types[0].type.name === "ice" && "bg-cyan-400",
           pokemon.types[0].type.name === "ghost" && "bg-purple-400",
           pokemon.types[0].type.name === "dragon" && "bg-purple-400",
           pokemon.types[0].type.name === "dark" && "bg-gray-400",
           pokemon.types[0].type.name === "shadow" && "bg-gray-400",
-          pokemon.types[0].type.name === "unknown" && "bg-gray-400"
+          pokemon.types[0].type.name === "unknown" && "bg-white"
         )}
       >
         <h1 className="text-xl capitalize text-center text-slate-900">
