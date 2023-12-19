5c566e99398c704b23068ebde6e12cea754c6caf
Vidhya Kumar <vidhya.kumar@outlook.com>
2023-12-19 12:43:26 +0800

    chore: disable poweredByHeader in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
   images: {
     minimumCacheTTL: 60 * 60 * 24 * 30,
     remotePatterns: [{ hostname: "raw.githubusercontent.com" }],
